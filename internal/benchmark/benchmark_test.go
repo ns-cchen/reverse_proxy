54d2b28c11351ae22220f600e5823f611efb116f
Chien-Ku Chen <cchen@netskope.com>
2024-08-22 16:08:57 +0800

    test

--- internal/benchmark/benchmark_test.go
+++ internal/benchmark/benchmark_test.go
@@ -1,7 +1,6 @@
 package benchmark
 
 import (
-	"fmt"
 	"io"
 	"math/rand"
 	"net/http"
@@ -10,57 +9,169 @@ import (
 	"time"
 )
 
-func BenchmarkApiConcurrent(b *testing.B) {
-	requestCounts := []int{1}
+func BenchmarkApiConcurrent10Request(b *testing.B) {
+	client := &http.Client{}
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
 
-	for _, count := range requestCounts {
-		b.Run(fmt.Sprintf("Requests-%d", count), func(b *testing.B) {
-			client := &http.Client{}
-
-			var wg sync.WaitGroup
-
-			for i := 0; i < count; i++ {
-				wg.Add(1)
-				go func() {
-					defer wg.Done()
-
-					response, err := client.Get("http://localhost:8000")
-					if err != nil {
-						b.Error(err)
-						return
-					}
-
-					// Read response in a streaming manner
-					buffer := make([]byte, 4096)
-					for {
-						_, err := response.Body.Read(buffer)
-
-						if err == io.EOF {
-							break
-						}
-						if err != nil {
-							b.Error(err)
-							return
-						}
-					}
-
-					err = response.Body.Close()
-					if err != nil {
-						b.Error(err)
-						return
-					}
-				}()
-				time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+			response, err := client.Get("http://localhost:8000")
+			if err != nil {
+				b.Error(err)
+				return
 			}
 
-			wg.Wait()
-		})
+			// Read response in a streaming manner
+			buffer := make([]byte, 4096)
+			for {
+				_, err := response.Body.Read(buffer)
+
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					b.Error(err)
+					return
+				}
+			}
+
+			err = response.Body.Close()
+			if err != nil {
+				b.Error(err)
+				return
+			}
+		}()
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
 	}
+
+	wg.Wait()
+
+}
+
+func BenchmarkApiConcurrent100Request(b *testing.B) {
+	client := &http.Client{}
+	var wg sync.WaitGroup
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			response, err := client.Get("http://localhost:8000")
+			if err != nil {
+				b.Error(err)
+				return
+			}
+
+			// Read response in a streaming manner
+			buffer := make([]byte, 4096)
+			for {
+				_, err := response.Body.Read(buffer)
+
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					b.Error(err)
+					return
+				}
+			}
+
+			err = response.Body.Close()
+			if err != nil {
+				b.Error(err)
+				return
+			}
+		}()
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+	}
+
+	wg.Wait()
+}
+
+func BenchmarkApiConcurrent1000Request(b *testing.B) {
+	client := &http.Client{}
+	var wg sync.WaitGroup
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			response, err := client.Get("http://localhost:8000")
+			if err != nil {
+				b.Error(err)
+				return
+			}
+
+			// Read response in a streaming manner
+			buffer := make([]byte, 4096)
+			for {
+				_, err := response.Body.Read(buffer)
+
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					b.Error(err)
+					return
+				}
+			}
+
+			err = response.Body.Close()
+			if err != nil {
+				b.Error(err)
+				return
+			}
+		}()
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+	}
+
+	wg.Wait()
+}
+
+func BenchmarkApiConcurrent10000Request(b *testing.B) {
+	client := &http.Client{}
+	var wg sync.WaitGroup
+	for i := 0; i < 10000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			response, err := client.Get("http://localhost:8000")
+			if err != nil {
+				b.Error(err)
+				return
+			}
+
+			// Read response in a streaming manner
+			buffer := make([]byte, 4096)
+			for {
+				_, err := response.Body.Read(buffer)
+
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					b.Error(err)
+					return
+				}
+			}
+
+			err = response.Body.Close()
+			if err != nil {
+				b.Error(err)
+				return
+			}
+		}()
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+	}
+
+	wg.Wait()
 }
 
 func BenchmarkApi(b *testing.B) {
 	client := &http.Client{}
-	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		response, err := client.Get("http://localhost:8000")
 		if err != nil {
