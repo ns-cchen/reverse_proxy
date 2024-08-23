6cf8f265cfb5342054f99d196fe75809bbd8602c
Chien-Ku Chen <cchen@netskope.com>
2024-08-23 08:47:46 +0800

    test

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -27,12 +27,36 @@ func ModifyResponse(response *http.Response) error {
 			return err
 		}
 
-		response.Body = io.NopCloser(reader)
+		response.Body = &gzipReaderCloser{
+			gzipReader: reader,
+			body:       response.Body,
+		}
 		response.Header.Del("Content-Encoding")
 		response.Header.Del("Content-Length")
 		response.Header.Set("Transfer-Encoding", "chunked")
 		response.ContentLength = -1
 		response.Uncompressed = true
 	}
+	return nil
+}
+
+type gzipReaderCloser struct {
+	gzipReader *gzip.Reader
+	body       io.Closer
+}
+
+func (grc *gzipReaderCloser) Read(p []byte) (n int, err error) {
+	return grc.gzipReader.Read(p)
+}
+
+func (grc *gzipReaderCloser) Close() error {
+	if err := grc.gzipReader.Close(); err != nil {
+		return err
+	}
+
+	if err := grc.body.Close(); err != nil {
+		return err
+	}
+
 	return nil
 }
